59ac8103e0ab16728fd0713d509af3b45c4915f5
sk.asim <sk.asim@BLRKEC400912L.ad.infosys.com>
2025-04-14 20:54:40 +0530

    partially no such element found exception when getting partner name

--- src/main/java/com/appsource/partnerDir/Scrapper.java
+++ src/main/java/com/appsource/partnerDir/Scrapper.java
@@ -10,11 +10,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Scrapper {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 		driver.get("https://appsource.microsoft.com/en-us/marketplace/partner-dir");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().window().maximize();
@@ -24,43 +27,39 @@ public class Scrapper {
 		int counter = driver.findElements(By.xpath("//div[@role='listitem']")).size();
 		String nextBtnXpath = "//button//span[text()='Next']";
 
-		int partnerCounter = 0;
 		try {
 			CSVPrinter csvPrinter = DataExporter.createCSVprinter(new File("ExtractedData.csv"));
 			csvPrinter.printComment("Egypt");
-//			try {
-//				Thread.sleep(Duration.ofSeconds(10));
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-			while(true) {
-				
+			
+			while(true) {		
 			for(int i=1;i<=counter;i++) {
-				List<Object> partnerDetails = Utils.getPartnerDetails(i,driver);
-						csvPrinter.printRecords(partnerDetails);
-						
-						
+				List<Object> partnerDetails = Utils.getPartnerDetails(i,driver,wait);
+				csvPrinter.printRecords(partnerDetails);
 				System.out.println("-------------------------------------");
 			}
-			csvPrinter.close();
 			try {
+				driver.switchTo().frame("partnersIframe");
+				wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(nextBtnXpath)));
 				if(!driver.findElement(By.xpath(nextBtnXpath)).isDisplayed()) {
 					System.out.println("Iterated through all the partners for this country");
+					csvPrinter.close();
 					break;
 				}
+				else {
+					driver.findElement(By.xpath(nextBtnXpath)).click();	
+				}
 			}
-			finally {
-				System.out.println("finally block");
-			}
-				driver.findElement(By.xpath(nextBtnXpath)).click();	
-			}
-		}
-			catch(NoSuchElementException | IOException e) {
+			catch(NoSuchElementException nsee) {
+				nsee.printStackTrace();
 				driver.switchTo().frame("partnersIframe");
 				driver.findElement(By.xpath(nextBtnXpath)).click();
-
 			}
+			
+			}
+		}
+			catch(Exception e) {
+				
+				e.printStackTrace();			}
 
 		}
 
